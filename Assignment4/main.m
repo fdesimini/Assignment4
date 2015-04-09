716991292a6a7f8b7e77cdf921241efe7da13149
fdesimini <fdesimini@users.noreply.github.com>
2015-04-09 00:08:35 -0400

    function to convert C to F

--- Assignment4/main.m
+++ Assignment4/main.m
@@ -8,10 +8,19 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
+//Write a program that has a function that converts 27° from degrees Fahrenheit (F) to degrees Celsius (C) using the following formula: C = (F - 32) / 1.8
+
+double convertFtoC (double fahrenheit) {
+    double celsius = (fahrenheit - 32) / 1.8;
+    return celsius;
+}
+
+int main (int argc, const char * argv[]){
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+
+        double fahrenheit = 27;
+        NSLog(@"27 degrees (F) converted to Celsius is %.02f", convertFtoC(fahrenheit));
+        
     }
     return 0;
 }
